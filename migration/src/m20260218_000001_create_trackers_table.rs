7d3a7b475506bdd2b06446d68f203568c7571f4d
junji <juliusj.puno@gmail.com>
2026-02-18 17:36:06 +0800

    I commit too often, people can't catch up lmao - Vaxry

--- migration/src/m20260218_000001_create_trackers_table.rs
+++ migration/src/m20260218_000001_create_trackers_table.rs
@@ -9,38 +9,38 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(Tracker::Table)
+                    .table(Trackers::Table)
                     .if_not_exists()
-                    .col(pk_auto(Tracker::Id).big_unsigned())
-                    .col(string(Tracker::Name))
-                    .col(text(Tracker::Desc))
+                    .col(pk_auto(Trackers::Id).big_unsigned())
+                    .col(string(Trackers::Name))
+                    .col(text(Trackers::Desc))
                     .col(
-                        timestamp(Tracker::CreatedAt)
+                        timestamp(Trackers::CreatedAt)
                             .not_null()
                             .default(Expr::current_timestamp()),
                     )
                     .col(
-                        timestamp(Tracker::UpdatedAt)
+                        timestamp(Trackers::UpdatedAt)
                             .not_null()
                             .default(Expr::current_timestamp()),
                     )
                     .index(
                         Index::create()
                             .name("idx_tracker_name")
-                            .table(Tracker::Table)
-                            .col(Tracker::Name),
+                            .table(Trackers::Table)
+                            .col(Trackers::Name),
                     )
                     .index(
                         Index::create()
                             .name("idx_created_at")
-                            .table(Tracker::Table)
-                            .col(Tracker::CreatedAt),
+                            .table(Trackers::Table)
+                            .col(Trackers::CreatedAt),
                     )
                     .index(
                         Index::create()
                             .name("idx_updated_at")
-                            .table(Tracker::Table)
-                            .col(Tracker::UpdatedAt),
+                            .table(Trackers::Table)
+                            .col(Trackers::UpdatedAt),
                     )
                     .to_owned(),
             )
@@ -49,13 +49,13 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Tracker::Table).to_owned())
+            .drop_table(Table::drop().table(Trackers::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Tracker {
+enum Trackers {
     Table,
     Id,
     Name,
